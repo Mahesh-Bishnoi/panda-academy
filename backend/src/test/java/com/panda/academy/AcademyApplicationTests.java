732ed11350044f4831a02f819044610cfe23ec53
Mahesh Bishnoi <bishnoimahesh96@gmail.com>
2025-11-06 19:51:52 +0100

    Initial spring project setup

---
+++ backend/src/test/java/com/panda/academy/AcademyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.panda.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
